ea8e32c3a1d42d5104ec70543f1d36f25b5be55c
SNAKE169 <ducanh6901@gmail.com>
2017-06-30 23:00:14 +0700

    a

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.send("asdas");
+  res.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
